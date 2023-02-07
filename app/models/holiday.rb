e5df90d641905c40205274c607e0dbd09918f855
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-02-07 16:37:11 +0100

    fix holiday all_in_month not using leap years

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -3,17 +3,13 @@ class Holiday < ApplicationRecord
   validates :date, presence: true
 
   def self.all_in_month(year, month)
-    days = Time.days_in_month month
+    days = Time.days_in_month month, year
     date_range = Date.new(year, month, 1)..Date.new(year, month, days)
-    where(date: date_range).order(:date).group_by do |holiday|
-      holiday.date
-    end
+    where(date: date_range).order(:date).group_by(&:date)
   end
 
   def self.all_in_year(year)
     date_range = Date.new(year, 1, 1)..Date.new(year, 12, 31)
-    where(date: date_range).order(:date).group_by do |holiday|
-      holiday.date
-    end
+    where(date: date_range).order(:date).group_by(&:date)
   end
 end
