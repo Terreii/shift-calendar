b619b899ef8b7e895563045af4137885f976c66a
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-02-12 00:01:38 +0100

    optimized PublicEvents#all_in_month and all_in_year

--- app/models/public_event.rb
+++ app/models/public_event.rb
@@ -3,28 +3,31 @@ class PublicEvent < ApplicationRecord
   validates :duration, presence: true
 
   # Queries all PublicEvents of the given month.
-  # Returns them in a Hash grouped by month
+  # Returns them in a Hash grouped by every date in their duration
   def self.all_in_month(year, month)
     days = Time.days_in_month month, year
     date_range = Date.new(year, month, 1)..Date.new(year, month, days)
-    where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range)).group_by { month }
+    events = Hash.new
+    where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range)).each do |event|
+      event.duration.each do |day|
+        events[day] = event
+      end
+    end
+    events
   end
 
   # Queries all PublicEvents of the given year.
-  # Returns them in a Hash grouped by month
+  # Returns them in a Hash grouped by every date in their duration
   def self.all_in_year(year)
     date_range = Date.new(year, 1, 1)..Date.new(year, 12, 31)
-    events = where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range))
-    months = Hash.new
-    (1..12).each do |month|
-      data = []
-      days = Time.days_in_month month, year
-      month_range = Date.new(year, month, 1)..Date.new(year, month, days)
-      events.each do |event|
-        data << event if event.duration.overlaps? month_range
+    events = Hash.new
+    where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range)).each do |event|
+      # grouped by Date should be faster, because there are less days with event, then days with events
+      # In the View it can then be looked up by events[Date]
+      event.duration.each do |day|
+        events[day] = event
       end
-      months[month] = data
     end
-    months
+    events
   end
 end
--- test/models/public_event_test.rb
+++ test/models/public_event_test.rb
@@ -3,19 +3,27 @@ require "test_helper"
 class PublicEventTest < ActiveSupport::TestCase
   test "all_in_month finds all PublicEvents of a month" do
     events = PublicEvent.all_in_month 2022, 8
-    assert_equal 1, events.size
-    assert_equal breaks(:multi_month_break).name, events[8].first.name
-    assert_instance_of Break, events[8].first
+    event = breaks(:multi_month_break)
+    assert_equal event, events[Date.new(2022, 8, 1)]
+    random_date = Faker::Date.in_date_period(year: 2022, month: 8)
+    assert_equal event, events[random_date], "random date"
+    assert_instance_of Break, events[random_date]
   end
 
   test "all_in_year finds all PublicEvents in a year" do
     events = PublicEvent.all_in_year 2022
-    assert_equal 12, events.size
-    assert_equal breaks(:break_in_month).name, events[6].first.name
-    # Event is in all of its months
-    assert_equal breaks(:multi_month_break).name, events[7].first.name
-    assert_equal breaks(:multi_month_break).name, events[8].first.name
-    assert_equal breaks(:multi_month_break).name, events[9].first.name
-    assert_equal breaks(:over_year_break).name, events[12].first.name
+
+    in_month_break = breaks(:break_in_month)
+    random_date = Faker::Date.between(from: in_month_break.duration.begin, to: in_month_break.duration.end)
+    assert_equal in_month_break, events[random_date]
+
+    summer_break = breaks(:multi_month_break)
+    random_date = Faker::Date.in_date_period(year: 2022, month: 8)
+    assert_equal summer_break, events[Date.new(2022, 7, 29)]
+    assert_equal summer_break, events[random_date]
+    assert_equal summer_break, events[Date.new(2022, 9, 11)]
+
+    winter_break = breaks(:over_year_break)
+    assert_equal winter_break, events[Date.new(2022, 12, 21)]
   end
 end
