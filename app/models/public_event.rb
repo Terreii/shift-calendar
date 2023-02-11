06a26e3cfb3708c1df81f4e80a2bc28874548e6f
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-02-11 23:25:39 +0100

    add all_in_month and all_in_year to public_event

--- app/models/public_event.rb
+++ app/models/public_event.rb
@@ -1,4 +1,30 @@
 class PublicEvent < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :duration, presence: true
+
+  # Queries all PublicEvents of the given month.
+  # Returns them in a Hash grouped by month
+  def self.all_in_month(year, month)
+    days = Time.days_in_month month, year
+    date_range = Date.new(year, month, 1)..Date.new(year, month, days)
+    where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range)).group_by { month }
+  end
+
+  # Queries all PublicEvents of the given year.
+  # Returns them in a Hash grouped by month
+  def self.all_in_year(year)
+    date_range = Date.new(year, 1, 1)..Date.new(year, 12, 31)
+    events = where("duration && ?::daterange", PublicEvent.connection.type_cast(date_range))
+    months = Hash.new
+    (1..12).each do |month|
+      data = []
+      days = Time.days_in_month month, year
+      month_range = Date.new(year, month, 1)..Date.new(year, month, days)
+      events.each do |event|
+        data << event if event.duration.overlaps? month_range
+      end
+      months[month] = data
+    end
+    months
+  end
 end
--- test/models/public_event_test.rb
+++ test/models/public_event_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PublicEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "all_in_month finds all PublicEvents of a month" do
+    events = PublicEvent.all_in_month 2022, 8
+    assert_equal 1, events.size
+    assert_equal breaks(:multi_month_break).name, events[8].first.name
+    assert_instance_of Break, events[8].first
+  end
+
+  test "all_in_year finds all PublicEvents in a year" do
+    events = PublicEvent.all_in_year 2022
+    assert_equal 12, events.size
+    assert_equal breaks(:break_in_month).name, events[6].first.name
+    # Event is in all of its months
+    assert_equal breaks(:multi_month_break).name, events[7].first.name
+    assert_equal breaks(:multi_month_break).name, events[8].first.name
+    assert_equal breaks(:multi_month_break).name, events[9].first.name
+    assert_equal breaks(:over_year_break).name, events[12].first.name
+  end
 end
