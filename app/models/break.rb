2c602ce8459ccdded11350d3f9b5be554f08bf5f
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-02-19 03:03:48 +0100

    rename Break to SchoolBreak

--- app/models/break.rb
+++
@@ -1,2 +0,0 @@
-class Break < PublicEvent
-end
--- test/models/public_event_test.rb
+++ test/models/public_event_test.rb
@@ -3,28 +3,28 @@ require "test_helper"
 class PublicEventTest < ActiveSupport::TestCase
   test "all_in_month finds all PublicEvents of a month" do
     events = PublicEvent.all_in_month 2022, 8
-    event = breaks(:multi_month_break)
+    event = school_breaks(:multi_month_break)
     assert_equal event, events[Date.new(2022, 8, 1)].first
 
     random_date = date_in_august_that_is_not_cat_day
     assert_equal event, events[random_date].first, "random date"
-    assert_instance_of Break, events[random_date].first
+    assert_instance_of SchoolBreak, events[random_date].first
   end
 
   test "all_in_year finds all PublicEvents in a year" do
     events = PublicEvent.all_in_year 2022
 
-    in_month_break = breaks(:break_in_month)
+    in_month_break = school_breaks(:break_in_month)
     random_date = Faker::Date.between(from: in_month_break.duration.begin, to: in_month_break.duration.end.yesterday)
     assert_equal in_month_break, events[random_date].first
 
-    summer_break = breaks(:multi_month_break)
+    summer_break = school_breaks(:multi_month_break)
     random_date = date_in_august_that_is_not_cat_day
     assert_equal summer_break, events[Date.new(2022, 7, 29)].first
     assert_equal summer_break, events[random_date].first
     assert_equal summer_break, events[Date.new(2022, 9, 11)].first
 
-    winter_break = breaks(:over_year_break)
+    winter_break = school_breaks(:over_year_break)
     assert_equal winter_break, events[Date.new(2022, 12, 21)].first
   end
 
