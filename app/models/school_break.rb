352524628dcddb77137c55bca3922a70aeeaab73
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-03-08 23:29:40 +0100

    annotate all models, fixtures and model tests

--- app/models/school_break.rb
+++ app/models/school_break.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: public_events
+#
+#  id         :bigint           not null, primary key
+#  duration   :daterange
+#  name       :string
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_public_events_on_duration           (duration) USING gist
+#  index_public_events_on_type               (type)
+#  index_public_events_on_type_and_duration  (type,duration)
+#
 class SchoolBreak < PublicEvent
 end
