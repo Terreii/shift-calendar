2987b468be077c6e0ac60064fdda681eae58ce1d
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-01-15 13:43:12 +0100

    refactor(resize_link_params): add missing callbacks

--- app/javascript/controllers/resize_link_params_controller.js
+++ app/javascript/controllers/resize_link_params_controller.js
@@ -21,19 +21,34 @@ export default class extends Controller {
 
     if (shouldSet !== this.shouldSetValue) {
       this.shouldSetValue = shouldSet
-
-      for (const link of this.linkTargets) {
-        if (!link.href) continue
-
-        const url = new URL(link.href)
-        if (shouldSet && !url.searchParams.has(this.nameValue)) {
-          url.searchParams.set(this.nameValue, true)
-        } else if(!shouldSet && url.searchParams.has(this.nameValue)) {
-          url.searchParams.delete(this.nameValue)
-        }
-
-        link.href = url.toString()
-      }
     }
+  }
+
+  breakpointValueChanged() {
+    this.resized()
+  }
+
+  shouldSetValueChanged() {
+    for (const link of this.linkTargets) {
+      if (!link.href) continue
+      this.setLinkParam(link)
+    }
+  }
+
+  linkTargetConnected(element) {
+    if (element.href) {
+      this.setLinkParam(element)
+    }
+  }
+
+  setLinkParam(link) {
+    const url = new URL(link.href)
+    if (this.shouldSetValue && !url.searchParams.has(this.nameValue)) {
+      url.searchParams.set(this.nameValue, true)
+    } else if(!this.shouldSetValue && url.searchParams.has(this.nameValue)) {
+      url.searchParams.delete(this.nameValue)
+    }
+
+    link.href = url.toString()
   }
 }
